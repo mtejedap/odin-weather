7ead8bf01c5c46637f43c7cbcaad32a3ddcaadac
mtejedap <mtejeda@purdue.edu>
2023-10-31 15:29:19 -0300

    Update DOM with API weather data

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
-function WeatherDay(condition, localTime, tempC, tempF, icon) {
-    this.condition = condition;
-    this.localTime = localTime;
-    this.tempC = tempC;
-    this.tempF = tempF;
-    this.icon = icon;
-}
-
 async function fetchLocation(location) {
     const response = await fetch(
         "https://api.weatherapi.com/v1/forecast.json" +
         "?key=e71c133aa6f547b5816151624233110&q=" + location
         + "&days=7");
     const weatherData = await response.json();
-    const name = weatherData.location.name;
-    const condition = weatherData.current.condition.text;
-    const localTime = weatherData.location.localtime;
-    const tempC = weatherData.current.temp_c;
-    const tempF = weatherData.current.temp_f;
-    const humidity = weatherData.current.humidity;
-    const windKph = weatherData.current.wind_kph;
-    const windMph = weatherData.current.wind_mph;
-    const icon = weatherData.current.condition.icon;
 
-    const weatherDays = new Array(6);
+    document.querySelector(".day-name-1").textContent = weatherData.forecast.forecastday[0].date;
+    document.querySelector(".temperature1").textContent = weatherData.forecast.forecastday[0].day.avgtemp_c
+                                + " °C / " + weatherData.forecast.forecastday[0].day.avgtemp_f
+                                + " °F";
+    document.querySelector(".condition1").textContent = weatherData.forecast.forecastday[0].day.condition.text;
 
     for (let i = 1; i < 7; i++) {
-        weatherDays[i - 1] = new WeatherDay(
-            weatherData.forecast.forecastday[i].day.condition.text,
-            weatherData.forecast.forecastday[i].astro.date,
-            weatherData.forecast.forecastday[i].day.avtemp_c,
-            weatherData.forecast.forecastday[i].day.avtemp_f,
-            weatherData.forecast.forecastday[i].day.condition.icon
-        );
+        let daySelector = ".day-name-" + (i + 1);
+        const day = document.querySelector(daySelector);
+        day.textContent = weatherData.forecast.forecastday[i].date;
+
+        const temperature = document.querySelector(".temperature" + (i + 1));
+        temperature.textContent = weatherData.forecast.forecastday[i].day.avgtemp_c
+                                  + " °C / " + weatherData.forecast.forecastday[i].day.avgtemp_f
+                                  + " °F";
+
+        const condition = document.querySelector(".condition" + (i + 1));
+        condition.textContent = weatherData.forecast.forecastday[i].day.condition.text;
     }
 
-    return { name, condition, localTime, tempC, tempF, humidity,
-             windKph, windMph, icon, weatherDays };
+    const city = document.querySelector(".city");
+    city.textContent = weatherData.location.name;
+
+    const condition = document.querySelector(".condition");
+    condition.textContent = weatherData.current.condition.text;
+
+    const date = document.querySelector(".date");
+    date.textContent = weatherData.location.localtime;
+
+    const temperature = document.querySelector(".temperature");
+    temperature.textContent = weatherData.current.temp_c + " °C / " + weatherData.current.temp_f + " °F";
+
+    const humidity = document.querySelector(".humidity");
+    humidity.textContent = weatherData.current.humidity + " %";
+
+    const wind = document.querySelector(".wind");
+    wind.textContent = weatherData.current.wind_kph + " kph / " + weatherData.current.wind_mph + " mph";
 }
 
 document.querySelector(".search-input").addEventListener("keyup", (e) => {
